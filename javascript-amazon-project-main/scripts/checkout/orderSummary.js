f2c826e1c1c16c3496410d0c447d3308d99e26ab
r3han <razarehan3391@gmail.com>
2026-04-14 04:21:55 +0530

    Make payment summary interactive

--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -1,12 +1,10 @@
 import {cart, removeFromCart, updateDeliveryOption} from '../../data/cart.js';
-import {products} from '../../data/products.js';
+import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import {deliveryOptions} from '../../data/deliveryOptions.js';
+import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import {renderPaymentSummary} from './paymentSummary.js';
 
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
 
 export function renderOrderSummary() {
   let cartSummaryHTML = '';
@@ -14,24 +12,11 @@ export function renderOrderSummary() {
   cart.forEach((cartItem) => {
       const productId = cartItem.productId;
 
-      let matchingProduct;
-
-      products.forEach((product) => {
-          if (product.id === productId) {
-              matchingProduct = product;
-          }
-      }); 
+      const matchingProduct = getProduct(productId);
 
       const deliveryOptionId = cartItem.deliveryOptionId;
 
-      let deliveryOption;
-
-      deliveryOptions.forEach((option) => {
-        if(option.id === deliveryOptionId){
-          deliveryOption = option;
-        }
-
-      });
+      const deliveryOption = getDeliveryOption(deliveryOptionId);
 
       const today = dayjs();
       const deliveryDate = today.add(
@@ -125,14 +110,20 @@ export function renderOrderSummary() {
 
   document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 
+  renderPaymentSummary();
+
   document.querySelectorAll('.js-delete-link')
   .forEach((link) => {
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
       removeFromCart(productId);
 
-      const container = document.querySelector(`.js-cart-item-container-${productId}`)
+      const container = document.querySelector(
+         `.js-cart-item-container-${productId}`
+      );
       container.remove();
+
+      renderOrderSummary();
     });
   });
 
@@ -142,6 +133,7 @@ export function renderOrderSummary() {
         const {productId, deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
       });
 
     });
